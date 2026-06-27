9944eb0a71f784692c498b51b7ff439aa00a0c84
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-06-28 02:52:52 +0900

    wip: fix GitHub Workflow

--- extras/scripts/vs-init.bat
+++ extras/scripts/vs-init.bat
@@ -11,34 +11,56 @@ IF /I "%~1" EQU "" (
 )
 
 SET XVS_BASE_COMMON=%ProgramFiles(x86)%\Microsoft Visual Studio
+SET XVS_BASE_COMMON64=%ProgramFiles%\Microsoft Visual Studio
+SET XVS_VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
 
 IF /I "%XVS_INIT_VER%" EQU "" GOTO SKIP_VER
 	REM need to do it this way so that () chars don't cause a syntax error
 	SET XVS_BASE_COMMON=%XVS_BASE_COMMON%\%XVS_INIT_VER%
+	SET XVS_BASE_COMMON64=%XVS_BASE_COMMON64%\%XVS_INIT_VER%
 :SKIP_VER
-
-IF NOT EXIST "%XVS_BASE_COMMON%" (
-	echo Visual Studio not installed or installed in a non-standard location!
-	exit /b 1
-)
 
 SET XVS_VCVARS_BAT=
 
+IF /I NOT "%XVS_INIT_VER%" EQU "" GOTO FIND_VCVARS_BY_DIR
+
+IF EXIST "%XVS_VSWHERE%" (
+	FOR /F "usebackq tokens=*" %%I IN (`"%XVS_VSWHERE%" -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -find VC\Auxiliary\Build\vcvarsall.bat`) DO (
+		echo + Found VCVARSALL.BAT at: %%I
+		SET XVS_VCVARS_BAT=%%I
+	)
+)
+
+IF /I NOT "%XVS_VCVARS_BAT%" EQU "" GOTO CALL_VCVARS
+
+:FIND_VCVARS_BY_DIR
+
 REM because the directories in theory should come out in order
 REM it'll automatically pick up the LAST vcvarsall.bat (in theory)
-FOR /F "usebackq tokens=*" %%I IN (`dir /b /on /s "%XVS_BASE_COMMON%\vcvarsall.bat"`) DO (
+FOR /F "usebackq tokens=*" %%I IN (`dir /b /on /s "%XVS_BASE_COMMON%\vcvarsall.bat" 2^>nul`) DO (
+	echo + Found VCVARSALL.BAT at: %%I
+	SET XVS_VCVARS_BAT=%%I
+)
+FOR /F "usebackq tokens=*" %%I IN (`dir /b /on /s "%XVS_BASE_COMMON64%\vcvarsall.bat" 2^>nul`) DO (
 	echo + Found VCVARSALL.BAT at: %%I
 	SET XVS_VCVARS_BAT=%%I
 )
 
+IF /I "%XVS_VCVARS_BAT%" EQU "" (
+	echo Visual Studio C++ build tools not found!
+	exit /b 1
+)
 
+:CALL_VCVARS
 echo == Using: %XVS_VCVARS_BAT% ==
 call "%XVS_VCVARS_BAT%" %~1
 
 
 
 SET XVS_BASE_COMMON=
+SET XVS_BASE_COMMON64=
 SET XVS_VCVARS_BAT=
+SET XVS_VSWHERE=
 
 
 exit /b 0
